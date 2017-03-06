8628610bbaef8258cefb5589ec1ee0efa9a9e30a
Meller008 <cs007@mail.ru>
2017-03-06 20:18:14 +0300

    Работа - доделан отчет по заказам

--- form/report_order.py
+++ form/report_order.py
@@ -23,11 +23,66 @@ class NeedArticleOrder(QMainWindow, need_article):
         super(NeedArticleOrder, self).__init__()
         self.setupUi(self)
         self.setWindowIcon(QIcon(getcwd() + "/images/icon.ico"))
+        self.start_settings()
+
+        self.select_id = []
+
+    def start_settings(self):
+        self.tw_article.horizontalHeader().resizeSection(0, 60)
+        self.tw_article.horizontalHeader().resizeSection(1, 50)
+        self.tw_article.horizontalHeader().resizeSection(2, 90)
+        self.tw_article.horizontalHeader().resizeSection(3, 240)
+        self.tw_article.horizontalHeader().resizeSection(4, 55)
+        self.tw_article.horizontalHeader().resizeSection(5, 65)
+        self.tw_article.horizontalHeader().resizeSection(6, 50)
+        self.tw_article.horizontalHeader().resizeSection(7, 65)
+
+    def sql_need_article(self):
+        query = """SELECT order_position.Product_Article_Parametr_Id, product_article.Article, product_article_size.Size, product_article_parametrs.Name,
+                        product_article_parametrs.Client_Name, SUM(order_position.Value), product_article_warehouse.Value_In_Warehouse AS w,
+                          IFNULL((SELECT SUM(pack.Value_Pieces)
+                          FROM pack
+                          WHERE Date_Make IS NULL AND pack.Article_Parametr_Id = order_position.Product_Article_Parametr_Id
+                          GROUP BY pack.Article_Parametr_Id), 0) AS c
+                      FROM order_position
+                        LEFT JOIN product_article_parametrs ON order_position.Product_Article_Parametr_Id = product_article_parametrs.Id
+                        LEFT JOIN product_article_size ON product_article_parametrs.Product_Article_Size_Id = product_article_size.Id
+                        LEFT JOIN product_article ON product_article_size.Article_Id = product_article.Id
+                        LEFT JOIN product_article_warehouse ON product_article_parametrs.Id = product_article_warehouse.Id_Article_Parametr
+                      WHERE order_position.Order_Id IN %s GROUP BY order_position.Product_Article_Parametr_Id""" % str(tuple(self.select_id)).replace(",)", ")")
+        sql_info = my_sql.sql_select(query)
+        if "mysql.connector.errors" in str(type(sql_info)):
+            QMessageBox.critical(self, "Ошибка sql получения артикулов", sql_info.msg, QMessageBox.Ok)
+            return False
+
+        for article in sql_info:
+            need_value = article[5] - article[6]
+            if need_value > 0:
+                self.tw_article.insertRow(self.tw_article.rowCount())
+                for column in range(1, len(article)):
+                    if isinstance(article[column], Decimal):
+                        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(article[column]))
+                    elif isinstance(article[column], datetime.date):
+                        text = article[column].strftime("%d.%m.%Y")
+                    else:
+                        text = str(article[column])
+                    item = QTableWidgetItem(text)
+                    item.setData(5, article[0])
+                    self.tw_article.setItem(self.tw_article.rowCount() - 1, column - 1, item)
+                else:
+                    text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(need_value))
+                    item = QTableWidgetItem(text)
+                    item.setData(5, article[0])
+                    self.tw_article.setItem(self.tw_article.rowCount() - 1, 7, item)
 
     def ui_view_filter_order(self):
-        self.filter_order = OrderFilter(self, True)
+        self.filter_order = OrderFilter(self, True, self.select_id)
         self.filter_order.setWindowModality(Qt.ApplicationModal)
         self.filter_order.show()
+
+    def of_select_order_id(self, id):
+        self.select_id = id
+        self.sql_need_article()
 
 
 class OrderFilter(table.TableList):
@@ -84,7 +139,10 @@ class OrderFilter(table.TableList):
                     text = str(table_typle[column])
                 item = QTableWidgetItem(text)
                 if column == 8:
-                    item.setCheckState(Qt.Unchecked)
+                    if table_typle[0] in self.other_value:
+                        item.setCheckState(Qt.Checked)
+                    else:
+                        item.setCheckState(Qt.Unchecked)
                 item.setData(5, table_typle[0])
                 self.table_widget.setItem(self.table_widget.rowCount() - 1, column - 1, item)
 
@@ -93,7 +151,12 @@ class OrderFilter(table.TableList):
 
     def ui_other(self):
         # отправить выделеные заказы
+        checked_id = []
         for row in range(self.table_widget.rowCount()):
             table_item = self.table_widget.item(row, 7)
             if table_item.checkState() == Qt.Checked:
-                print(table_item.data(5))
+                checked_id.append(table_item.data(5))
+
+        self.main.of_select_order_id(checked_id)
+        self.close()
+        self.destroy()
