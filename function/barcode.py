eab8b2c18f7e909a699cd9166652878bee31a152
Meller008 <cs007@mail.ru>
2017-04-18 16:32:19 +0300

    работа - отремонтирован печать пасспорта!

--- function/barcode.py
+++ function/barcode.py
@@ -1,6 +1,7 @@
 import barcode
+from os import getcwd
 
 
 def generate(cod):
     EAN = barcode.Code39(cod, add_checksum=False)
-    EAN.save("temp/%s" % cod, options={'module_height': 5.0})
+    EAN.save(getcwd() + "/temp/%s" % cod, options={'module_height': 5.0})
