81a91a70d8e70103f3caf0359c3f5160c201c602
Meller008 <cs007@mail.ru>
2017-08-09 18:02:03 +0300

    работа - Исправлен вывод ошибки!

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from PyQt5.QtWidgets import QApplication
-import sys, traceback
+import sys, traceback, time
 import form.main_window
 from datetime import datetime
 
-
 def ex(t, v, tb):
+    print(traceback.print_exception(t, v, tb))
     with open("ERR.txt", "a") as file:
         file.write("\n---===Error===---\n")
         file.write("Time = %s" % datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
         traceback.print_exception(t, v, tb, file=file)
-        input("ПАУЗА5")
-        input("ПАУЗА4")
-        input("ПАУЗА3")
-        input("ПАУЗА2")
-        input("ПАУЗА1")
+    while 1:
+        time.sleep(5)
 
 sys.excepthook = ex
 
-app = QApplication(sys.argv)
-main = form.main_window.MainWindow()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = form.main_window.MainWindow()
+    sys.exit(app.exec_())
 
