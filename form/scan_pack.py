8367de887ce70a07cf9ffe2c0e6f738c7c9b0cd6
Meller008 <cs007@mail.ru>
2017-09-28 16:14:50 +0300

    работа - Поправлен сканер пачек!

--- form/scan_pack.py
+++ form/scan_pack.py
@@ -1,8 +1,9 @@
 from os import getcwd
 from PyQt5.uic import loadUiType
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMessageBox
 from PyQt5.QtGui import QIcon
 from form import pack
+from function import my_sql
 
 
 scan_pack = loadUiType(getcwd() + '/ui/scan_pack.ui')[0]
@@ -18,6 +19,16 @@ class ScanPack(QDialog, scan_pack):
         try:
             id = int(self.le_number.text())
         except:
+            return False
+
+        query = """SELECT COUNT(Id) FROM pack WHERE Id = %s"""
+        count_id = my_sql.sql_select(query, (id, ))
+        if "mysql.connector.errors" in str(type(count_id)):
+            QMessageBox.critical(self, "Ошибка sql поиска пачки", count_id.msg, QMessageBox.Ok)
+            return False
+
+        if count_id[0][0] != 1:
+            QMessageBox.information(self, "Ошибка номера", "Нет пачки с таким номером", QMessageBox.Ok)
             return False
 
         self.pack = pack.PackBrows(pack_id=id)
