d790b4f5048729929681c4b8c61280de9f93f3ec
Meller008 <cs007@mail.ru>
2017-10-09 17:36:00 +0300

    работа - Поправлен пасспорт при печати из кроя!

--- form/cut.py
+++ form/cut.py
@@ -751,16 +751,16 @@ class CutPassport(QDialog, cut_print_passport):
                             <td style="width: 60px; height: 10px; text-align: center; vertical-align: middle;">Размер</td>
                             <td style="width: 60px; height: 10px; text-align: center; vertical-align: middle;">Крой</td>
                             <td style="width: 60px; height: 10px; text-align: center; vertical-align: middle;">Пачка</td>
-                            <td style="width: 130px; height: 10px; text-align: center; vertical-align: middle;">Дата</td>
                             <td style="width: 160px; height: 10px; text-align: center; vertical-align: middle;">ID пачки</td>
+                            <td style="width: 130px; height: 10px; text-align: center; vertical-align: middle;">Дата</td>
                             </tr>
                             <tr>
                             <td style="height: 10px; font-size: 15pt; text-align: center; vertical-align: middle;"><strong>#art#</strong></td>
                             <td style="width: 60px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;"><strong>#size#</strong></td>
                             <td style="width: 60px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;"><strong>#cut#</strong></td>
                             <td style="width: 60px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;"><strong>#pack#</strong></td>
-                            <td style="width: 130px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;"><strong>#data#</strong></td>
                             <td style="width: 160px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;" rowspan="3"><strong><img src="#pac_barcode_src#" alt="lorem" width="150" height="70" /></strong></td>
+                            <td style="width: 130px; height: 10px; font-size: 18pt; text-align: center; vertical-align: middle;"><strong>#data#</strong></td>
                             </tr>
                             <tr>
                             <td style="height: 10px; text-align: center; vertical-align: middle;">Название</td>
