97534e9be4a57516a98e6262eff3efad817e0cb8
Meller008 <cs007@mail.ru>
2017-10-24 13:46:46 +0300

    работа - Тест штрихкода

--- function/barcode.py
+++ function/barcode.py
@@ -4,4 +4,4 @@ from os import getcwd
 
 def generate(cod):
     EAN = barcode.Code39(cod, add_checksum=False)
-    EAN.save(getcwd() + "/temp/%s" % cod, options={'module_height': 5.0})
+    EAN.save(getcwd() + "/temp/%s" % cod, options={'module_height': 7.0})
