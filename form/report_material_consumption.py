961853ec710598b5a044dbe16101f09479d6383e
Meller008 <cs007@mail.ru>
2017-11-15 14:32:04 +0300

    Работа - Сделан отчет расхода фурнитуры!

--- form/report_material_consumption.py
+++ form/report_material_consumption.py
@@ -23,6 +23,8 @@ class ReportMaterialConsumption(QMainWindow, material_consumption_class):
     def start_settings(self):
         self.de_material_from.setDate(QDate.currentDate().addMonths(-1))
         self.de_material_to.setDate(QDate.currentDate())
+        self.de_accessories_from.setDate(QDate.currentDate().addMonths(-1))
+        self.de_accessories_to.setDate(QDate.currentDate())
 
         self.tw_material.horizontalHeader().resizeSection(0, 150)
         self.tw_material.horizontalHeader().resizeSection(1, 70)
@@ -33,6 +35,16 @@ class ReportMaterialConsumption(QMainWindow, material_consumption_class):
         self.tw_material_info.horizontalHeader().resizeSection(0, 90)
         self.tw_material_info.horizontalHeader().resizeSection(1, 80)
         self.tw_material_info.horizontalHeader().resizeSection(2, 80)
+
+        self.tw_accessories.horizontalHeader().resizeSection(0, 150)
+        self.tw_accessories.horizontalHeader().resizeSection(1, 85)
+        self.tw_accessories.horizontalHeader().resizeSection(2, 80)
+        self.tw_accessories.horizontalHeader().resizeSection(3, 85)
+        self.tw_accessories.horizontalHeader().resizeSection(4, 80)
+
+        self.tw_accesories_info.horizontalHeader().resizeSection(0, 90)
+        self.tw_accesories_info.horizontalHeader().resizeSection(1, 80)
+        self.tw_accesories_info.horizontalHeader().resizeSection(2, 80)
 
     def ui_calc_material(self):
         # Расчет общего расхода ткани
@@ -260,3 +272,130 @@ class ReportMaterialConsumption(QMainWindow, material_consumption_class):
         text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(sql_info[1], 2)))
         item = QTableWidgetItem(text)
         self.tw_material_info.setItem(self.tw_material_info.rowCount() - 1, 2, item)
+
+    def ui_calc_accessories(self):
+        # Расчет общего расхода Фурнитуры
+        query = """SELECT accessories_name.Id, accessories_name.Name, SUM(accessories_supplyposition.Value), 
+                        SUM(accessories_supplyposition.Value * accessories_supplyposition.Price)
+                      FROM accessories_supply LEFT JOIN accessories_supplyposition ON accessories_supply.Id = accessories_supplyposition.Accessories_SupplyId
+                        LEFT JOIN accessories_name ON accessories_supplyposition.Accessories_NameId = accessories_name.Id
+                      WHERE accessories_supply.Data >= %s and accessories_supply.Data <= %s
+                      GROUP BY accessories_name.Id"""
+        sql_material_in = my_sql.sql_select(query, (self.de_accessories_from.date().toString(Qt.ISODate), self.de_accessories_to.date().toString(Qt.ISODate)))
+        if "mysql.connector.errors" in str(type(sql_material_in)):
+            QMessageBox.critical(self, "Ошибка sql получения приходов Фурнитуры", sql_material_in.msg, QMessageBox.Ok)
+            return False
+
+        query = """SELECT accessories_name.Id, accessories_name.Name, SUM(transaction_records_accessories.Balance),
+                        SUM(transaction_records_accessories.Balance * accessories_supplyposition.Price)
+                      FROM transaction_records_accessories LEFT JOIN accessories_balance ON transaction_records_accessories.Supply_Balance_Id = accessories_balance.Id
+                      LEFT JOIN accessories_supplyposition ON accessories_balance.accessories_SupplyPositionId = accessories_supplyposition.Id
+                        LEFT JOIN accessories_supply ON accessories_supplyposition.accessories_SupplyId = accessories_supply.Id
+                        LEFT JOIN accessories_name ON accessories_supplyposition.accessories_NameId = accessories_name.Id
+                      WHERE transaction_records_accessories.Note NOT LIKE 'Заказ %' AND transaction_records_accessories.Note NOT LIKE '%нарезка%'
+                        AND transaction_records_accessories.Date >= %s AND transaction_records_accessories.Date <= %s
+                      GROUP BY accessories_name.Id"""
+        sql_material_out = my_sql.sql_select(query, (self.de_accessories_from.date().toString(Qt.ISODate), self.de_accessories_to.date().toString(Qt.ISODate)))
+        if "mysql.connector.errors" in str(type(sql_material_out)):
+            QMessageBox.critical(self, "Ошибка sql получения расходов фурнитуры", sql_material_out.msg, QMessageBox.Ok)
+            return False
+
+        all_list_consumption = []
+        for out in sql_material_out:
+            fillst = list(filter(lambda s: s[0] == out[0], sql_material_in))
+
+            if fillst:
+                newlst = [out[0], out[1], fillst[0][2], fillst[0][3], out[2], out[3]]
+            else:
+                newlst = [out[0], out[1], 0, 0, out[2], out[3]]
+
+            all_list_consumption.append(newlst)
+
+        self.tw_accessories.clearContents()
+        self.tw_accessories.setRowCount(0)
+
+        all_weight_in = 0
+        all_weight_out = 0
+        all_sum_in = 0
+        all_sum_out = 0
+
+        for i in all_list_consumption:
+            self.tw_accessories.insertRow(self.tw_accessories.rowCount())
+
+            item = QTableWidgetItem(str(i[1]))
+            item.setData(5, i[0])
+            self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 0, item)
+
+            all_weight_in += i[2]
+            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(i[2], 2)))
+            item = QTableWidgetItem(text)
+            item.setData(5, i[0])
+            self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 1, item)
+
+            all_sum_in += i[3]
+            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(i[3], 2)))
+            item = QTableWidgetItem(text)
+            item.setData(5, i[0])
+            self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 2, item)
+
+            all_weight_out += i[4]
+            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(i[4], 2)))
+            item = QTableWidgetItem(text)
+            item.setData(5, i[0])
+            self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 3, item)
+
+            all_sum_out += i[5]
+            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(i[5], 2)))
+            item = QTableWidgetItem(text)
+            item.setData(5, i[0])
+            self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 4, item)
+
+        self.tw_accessories.insertRow(self.tw_accessories.rowCount())
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(all_weight_in, 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 1, item)
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(all_sum_in, 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 2, item)
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(all_weight_out, 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 3, item)
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(all_sum_out, 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accessories.setItem(self.tw_accessories.rowCount() - 1, 4, item)
+
+    def ui_calc_accessories_info(self):
+        # Расчет подробный фурнитуры
+        self.tw_accesories_info.clearContents()
+        self.tw_accesories_info.setRowCount(0)
+
+        filter_date = (self.de_accessories_from.date().toString(Qt.ISODate), self.de_accessories_to.date().toString(Qt.ISODate))
+
+        query = """SELECT SUM(transaction_records_accessories.Balance),
+                        SUM(transaction_records_accessories.Balance * accessories_supplyposition.Price)
+                      FROM transaction_records_accessories
+                        LEFT JOIN accessories_balance ON transaction_records_accessories.Supply_Balance_Id = accessories_balance.Id
+                        LEFT JOIN accessories_supplyposition ON accessories_balance.accessories_SupplyPositionId = accessories_supplyposition.Id
+                      WHERE Note LIKE '%пачк%'
+                        AND transaction_records_accessories.Date >= %s AND transaction_records_accessories.Date <= %s"""
+        sql_info = my_sql.sql_select(query, filter_date)[0]
+        if "mysql.connector.errors" in str(type(sql_info)):
+            QMessageBox.critical(self, "Ошибка sql получения расхода на пачки", sql_info.msg, QMessageBox.Ok)
+            return False
+
+        self.tw_accesories_info.insertRow(self.tw_accesories_info.rowCount())
+
+        item = QTableWidgetItem("Пачки")
+        self.tw_accesories_info.setItem(self.tw_accesories_info.rowCount() - 1, 0, item)
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(sql_info[0], 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accesories_info.setItem(self.tw_accesories_info.rowCount() - 1, 1, item)
+
+        text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(sql_info[1], 2)))
+        item = QTableWidgetItem(text)
+        self.tw_accesories_info.setItem(self.tw_accesories_info.rowCount() - 1, 2, item)
