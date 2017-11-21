6b0c073ada915b5f445d5b276df90f9835e3134d
Meller008 <cs007@mail.ru>
2017-11-21 18:41:47 +0300

    Работа - Доделан отчет по прибыли!

--- form/report_profit.py
+++ form/report_profit.py
@@ -1,15 +1,13 @@
 from os import getcwd
 from PyQt5.uic import loadUiType
-from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem, QMainWindow
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem, QMainWindow, QFileDialog
 from PyQt5.QtCore import Qt, QDate
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QBrush, QColor
 import re
-from decimal import Decimal
-import datetime
 from function import my_sql
-from form.templates import table
+from function import table_to_html, to_excel
+from classes import print_qt
 
-import time
 
 
 report_profit_class = loadUiType(getcwd() + '/ui/report_profit.ui')[0]
@@ -26,6 +24,12 @@ class ReportProfit(QMainWindow, report_profit_class):
     def set_start_settings(self):
         self.de_date_from.setDate(QDate.currentDate().addMonths(-1))
         self.de_date_to.setDate(QDate.currentDate())
+
+        self.tableWidget.horizontalHeader().resizeSection(0, 150)
+        self.tableWidget.horizontalHeader().resizeSection(1, 70)
+        self.tableWidget.horizontalHeader().resizeSection(2, 90)
+        self.tableWidget.horizontalHeader().resizeSection(3, 80)
+        self.tableWidget.horizontalHeader().resizeSection(4, 80)
 
     def ui_calc(self):
 
@@ -54,10 +58,11 @@ class ReportProfit(QMainWindow, report_profit_class):
             article_list[order_position[0]]["value"] += order_position[2]
             article_list[order_position[0]]["sum"] += order_position[2] * order_position[3]
 
-        a = time.time()
-
         # Получаем id разных пачек для подсчета
         for key, value in article_list.items():
+
+            self.statusBar.showMessage("Расчет артикула %s" % article_list[key]["name"])
+
             query = """SELECT pack.Id FROM pack LEFT JOIN cut ON pack.Cut_Id = cut.Id JOIN (SELECT @sum := 0) t
                           WHERE pack.Article_Parametr_Id = %s AND cut.Date_Cut <= %s AND (@sum := @sum + pack.Value_Pieces) <= %s"""
             sql_pack_id_info = my_sql.sql_select(query, (key, self.de_date_to.date().toPyDate(), value["value"] + 200))
@@ -76,7 +81,7 @@ class ReportProfit(QMainWindow, report_profit_class):
                 pack_id = sql_pack_id_info[i][0]
                 # Узнаем всю себестоимость
                 query = """SELECT(
-                                  SELECT AVG(DISTINCT material_supplyposition.Price) * (SELECT (pack.Weight * (1 + cut.Rest_Percent / 100)) / Pack_Value
+                                  SELECT AVG(DISTINCT material_supplyposition.Price) * (SELECT (pack.Weight * (1 + cut.Rest_Percent / 100)) / pack.Value_Pieces
                                                                                         FROM pack LEFT JOIN cut ON pack.Cut_Id = cut.Id
                                                                                         WHERE pack.Id = pm.Id)
                                   FROM pack LEFT JOIN transaction_records_material ON transaction_records_material.Cut_Material_Id = pack.Cut_Id
@@ -84,7 +89,9 @@ class ReportProfit(QMainWindow, report_profit_class):
                                     LEFT JOIN material_supplyposition ON material_balance.Material_SupplyPositionId = material_supplyposition.Id
                                   WHERE pack.Id = pm.Id AND transaction_records_material.Note NOT LIKE '%доп. тк%'
                                   ),(
-                                      SELECT (Weight + Weight_Rest) * Price FROM pack_add_material WHERE Pack_Id = pm.Id
+                                      SELECT ((pack_add_material.Weight + pack_add_material.Weight_Rest) / pack.Value_Pieces) * pack_add_material.Price
+                                        FROM pack_add_material LEFT JOIN pack ON pack_add_material.Pack_Id = pack.Id
+                                        WHERE pack.Id = pm.Id
                                   ),(
                                       SELECT SUM(avg) FROM (
                                         SELECT AVG(accessories_supplyposition.Price) * pack_accessories.Value_Thing AS avg
@@ -114,95 +121,83 @@ class ReportProfit(QMainWindow, report_profit_class):
 
             self.tableWidget.insertRow(self.tableWidget.rowCount())
 
+            # Цвет зависит от положительной или отрицательной прибыли
+            if article_list[key]["seb"]:
+                profit = (article_list[key]["sum"] - (article_list[key]["seb"] * article_list[key]["value"]))
+                if profit > 0:
+                    color = QBrush(QColor(150, 255, 161, 255))
+                else:
+                    color = QBrush(QColor(255, 255, 153, 255))
+            else:
+                color = QBrush(QColor(252, 141, 141, 255))
+
             item = QTableWidgetItem(article_list[key]["name"])
+            item.setBackground(color)
             self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 0, item)
 
             text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(article_list[key]["value"]))
             item = QTableWidgetItem(text)
+            item.setBackground(color)
             self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 1, item)
 
-            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(article_list[key]["sum"], 2)))
+            text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(article_list[key]["sum"], 4)))
             item = QTableWidgetItem(text)
+            item.setBackground(color)
             self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 2, item)
 
             if article_list[key]["seb"]:
-                text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(article_list[key]["seb"], 2)))
+                text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(article_list[key]["seb"], 4)))
             else:
                 text = "None"
             item = QTableWidgetItem(text)
+            item.setBackground(color)
             self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 3, item)
 
+            if article_list[key]["seb"]:
+                text = re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(profit, 4)))
+            else:
+                text = "None"
+            item = QTableWidgetItem(text)
+            item.setBackground(color)
+            self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 4, item)
 
+    def ui_calc_sum(self):
+        value_sum, sold_sum, seb_sum = 0, 0, 0
+        for row in range(self.tableWidget.rowCount()):
+            value_row = int(self.tableWidget.item(row, 1).text().replace(" ", ""))
+            sold_row = float(self.tableWidget.item(row, 2).text().replace(" ", ""))
+            value_sum += value_row
+            sold_sum += sold_row
 
+            if self.tableWidget.item(row, 3).text() != "None":
+                try:
+                    seb_row = float(self.tableWidget.item(row, 3).text().replace(",", ".").replace(" ", ""))
+                    seb_sum += (value_row * seb_row)
+                except:
+                    QMessageBox.critical(self, "Ошибка Себестоимости", "Ошибка в артикуле %s" % self.tableWidget.item(row, 0).text(), QMessageBox.Ok)
+                    return False
+        self.le_value_sum.setText(re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(value_sum, 4))))
+        self.le_sold_sum.setText(re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(sold_sum, 4))))
+        self.le_seb_sum.setText(re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(seb_sum, 4))))
+        self.le_profit_sum.setText(re.sub(r'(?<=\d)(?=(\d\d\d)+\b.)', ' ', str(round(sold_sum - seb_sum, 4))))
 
-            #     # Узнаем цену ткани кроя (Если перекресная то просто берем среднюю а+б/2)
-            #     query = """SELECT AVG(DISTINCT material_supplyposition.Price)
-            #                   FROM pack LEFT JOIN transaction_records_material ON transaction_records_material.Cut_Material_Id = pack.Cut_Id
-            #                     LEFT JOIN material_balance ON transaction_records_material.Supply_Balance_Id = material_balance.Id
-            #                     LEFT JOIN material_supplyposition ON material_balance.Material_SupplyPositionId = material_supplyposition.Id
-            #                   WHERE pack.Id = %s AND transaction_records_material.Note NOT LIKE '%доп. тк%'"""
-            #     sql_info = my_sql.sql_select(query, (pack_id, ))
-            #     if "mysql.connector.errors" in str(type(sql_info)):
-            #         QMessageBox.critical(self, "Ошибка sql получения средней цены ткани для кроя", sql_info.msg, QMessageBox.Ok)
-            #         return False
-            #
-            #     material_price = sql_info[0][0]
-            #
-            #     # Узнаем вес пачки и % обрези и кол-во в пачке
-            #     query = """SELECT pack.Weight, cut.Rest_Percent, pack.Value_Pieces FROM pack LEFT JOIN cut ON pack.Cut_Id = cut.Id WHERE pack.Id = %s"""
-            #     sql_info = my_sql.sql_select(query, (pack_id, ))
-            #     if "mysql.connector.errors" in str(type(sql_info)):
-            #         QMessageBox.critical(self, "Ошибка sql получения вес пачки и % обрези", sql_info.msg, QMessageBox.Ok)
-            #         return False
-            #
-            #     # Считаем цену за ткань
-            #     pack_value = sql_info[0][2]
-            #     pack_material_sum = ((sql_info[0][0] * (1 + sql_info[0][1] / 100)) * material_price) / pack_value
-            #
-            #     # Считаем доп. ткань
-            #     query = """SELECT (Weight + Weight_Rest) * Price FROM pack_add_material WHERE Pack_Id = %s"""
-            #     sql_info = my_sql.sql_select(query, (pack_id, ))
-            #     if "mysql.connector.errors" in str(type(sql_info)):
-            #         QMessageBox.critical(self, "Ошибка sql получения доп. ткань", sql_info.msg, QMessageBox.Ok)
-            #         return False
-            #
-            #     if sql_info:
-            #         pack_a_material_sum = sql_info[0][0] / pack_value
-            #     else:
-            #         pack_a_material_sum = 0
-            #
-            #     # Считаем фурнитуру
-            #     query = """SELECT SUM(avg) FROM (
-            #                   SELECT AVG(accessories_supplyposition.Price) * pack_accessories.Value_Thing AS avg
-            #                   FROM pack
-            #                     LEFT JOIN pack_accessories ON pack.Id = pack_accessories.Pack_Id
-            #                     LEFT JOIN transaction_records_accessories ON transaction_records_accessories.Pack_Accessories_Id = pack_accessories.Id
-            #                     LEFT JOIN accessories_balance ON transaction_records_accessories.Supply_Balance_Id = accessories_balance.Id
-            #                     LEFT JOIN accessories_supplyposition ON accessories_balance.Accessories_SupplyPositionId = accessories_supplyposition.Id
-            #                   WHERE pack.Id = %s
-            #                   GROUP BY pack_accessories.Id) t"""
-            #     sql_info = my_sql.sql_select(query, (pack_id, ))
-            #     if "mysql.connector.errors" in str(type(sql_info)):
-            #         QMessageBox.critical(self, "Ошибка sql получения фурнитуры", sql_info.msg, QMessageBox.Ok)
-            #         return False
-            #
-            #     pack_accessories_sum = sql_info[0][0]
-            #
-            #     # Считаем операции
-            #     query = """SELECT SUM(pack_operation.Price) FROM pack_operation WHERE pack_operation.Pack_Id = %s"""
-            #     sql_info = my_sql.sql_select(query, (pack_id, ))
-            #     if "mysql.connector.errors" in str(type(sql_info)):
-            #         QMessageBox.critical(self, "Ошибка sql получения суммы операций", sql_info.msg, QMessageBox.Ok)
-            #         return False
-            #
-            #     pack_operation_sum = sql_info[0][0]
-            #
-            #     sebest_pack_list.append((pack_material_sum + pack_a_material_sum + pack_accessories_sum + pack_operation_sum))
-            #
-            # if sebest_pack_list:
-            #     article_list[key]["seb"] = sum(sebest_pack_list) / len(sebest_pack_list)
-            # else:
-            #     article_list[key]["seb"] = None
+    def ui_print(self):
+        up_html = """
+          <table>
+          <tr> <th>Продано товара</th><th>На сумму</th><th>Затрачено рублей</th><th>ИТОГО прибыль</th> </tr>
+          <tr> <td>#value_sum#</td><td>#sold_sum#</td><td>#seb_sum#</td><td>#all_sum#</td> </tr>
+          </table>"""
+        up_html = up_html.replace("#value_sum#", self.le_value_sum.text())
+        up_html = up_html.replace("#sold_sum#", self.le_sold_sum.text())
+        up_html = up_html.replace("#seb_sum#", self.le_seb_sum.text())
+        up_html = up_html.replace("#all_sum#", self.le_profit_sum.text())
 
+        head = "Прибыль предприятия %s-%s" % (self.de_date_from.date().toString(Qt.ISODate), self.de_date_to.date().toString(Qt.ISODate))
 
-        print(time.time() - a)
+        html = table_to_html.tab_html(self.tableWidget, table_head=head, up_template=up_html)
+        self.print_class = print_qt.PrintHtml(self, html)
+
+    def ui_export(self):
+        path = QFileDialog.getSaveFileName(self, "Сохранение")
+        if path[0]:
+            to_excel.table_to_excel(self.tableWidget, path[0])
