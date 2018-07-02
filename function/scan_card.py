3fa995be2aa09ee9e8f5509319f0455fc43146de
Meller008 <cs007@mail.ru>
2018-07-02 14:24:42 +0300

    Сервер-Исправлена ошибка

--- function/scan_card.py
+++ function/scan_card.py
@@ -1,10 +1,10 @@
-import time
-try:
-    import serial
-except ImportError:
-    import pip
-    pip.main(['install', 'pyserial'])
-    import serial
+#import time
+#try:
+#    import serial
+#except ImportError:
+#    import pip
+#    pip.main(['install', 'pyserial'])
+#    import serial
 
 
 def scan():
@@ -12,10 +12,10 @@ def scan():
     serial_arduino = None
     i = 0
     while i < 5:
-        try:
-            serial_arduino = serial.Serial('COM3', 9600, timeout=2)
-            i = 6
-        except:
+#        try:
+#            serial_arduino = serial.Serial('COM3', 9600, timeout=2)
+#            i = 6
+#        except:
             i += 1
             time.sleep(1)
 
