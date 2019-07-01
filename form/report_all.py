367dcd86e8e67213a4923bf6c6279b6a0e27375d
Meller008 <cs007@mail.ru>
2019-07-01 17:44:31 +0300

    Работа - Правка полного отчета

--- form/report_all.py
+++ form/report_all.py
@@ -1319,14 +1319,14 @@ class ReportAll(QMainWindow):
                 article_list.update(new)
 
         # Получим проданые артикула
-        query = """SELECT product_article_parametrs.Id, CONCAT(product_article.Article, '(', product_article_size.Size, ')[', product_article_parametrs.Name, ']'),
+        query = """ SELECT product_article_parametrs.Id, CONCAT(product_article.Article, '(', product_article_size.Size, ')[', product_article_parametrs.Name, ']'),
                         order_position.Value, IF(clients.No_Nds, order_position.Price * (order_position.NDS / 100 + 1), order_position.Price), product_article_parametrs.Price
                       FROM order_position LEFT JOIN `order` ON order_position.Order_Id = `order`.Id
                         LEFT JOIN clients ON `order`.Client_Id = clients.Id
                         LEFT JOIN product_article_parametrs ON order_position.Product_Article_Parametr_Id = product_article_parametrs.Id
                         LEFT JOIN product_article_size ON product_article_parametrs.Product_Article_Size_Id = product_article_size.Id
                         LEFT JOIN product_article ON product_article_size.Article_Id = product_article.Id
-                      WHERE `order`.Date_Shipment >= %s AND `order`.Date_Shipment <= %s AND `order`.Shipped = 1"""
+                      WHERE `order`.Date_Shipment >= %s AND `order`.Date_Shipment <= %s """
         sql_info = my_sql.sql_select(query, (self.de_date_from.date().toPyDate(), self.de_date_to.date().toPyDate()))
         if "mysql.connector.errors" in str(type(sql_info)):
             QMessageBox.critical(self, "Ошибка sql получения проданых позиций", sql_info.msg, QMessageBox.Ok)
